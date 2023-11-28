af60d67888b8e89250ba1cb2c1a800689d076b21
abk-gami <yahabubakar01@gmail.com>
2023-11-28 14:38:37 +0100

    e

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
-import { StyleSheet, Text, View, Modal, Button} from 'react-native'
+import { StyleSheet, Text, View, ActivityIndicator} from 'react-native'
 import React, {useState} from 'react';
 import { StatusBar } from 'react-native';
+import { Button } from 'react-native';
 
 
 const App = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [showStatus, setShowStatus] = useState(false)
   return (
     <View style={{backgroundColor: '#ff0000', flex: 1,}}>
-      <StatusBar backgroundColor={'green'} barStyle={'default'}  />
+      <StatusBar backgroundColor={'green'} barStyle={'default'}  hidden={showStatus}/>
 
-      <Button 
-      title='Open Modal'
-      onPress={() => setIsModalVisible(true)}
-      />
-
+      <ActivityIndicator size={"large"} color={'black '} animating={isLoading}/>
       
-
-
-
       <Text style={{color: '#a9a9f3',}}>Hello World!</Text>
+  
+       <Button 
+       color={'#00ff73'}
+       title='Show'
+       onPress={()=>  setIsLoading(true)} 
+      />
+       <Button 
+       color={'#00ff73'}
+       title='Hide'
+       onPress={()=> setIsLoading(false)}
+      />
+ 
+
+
+
     </View>
   )
 }
