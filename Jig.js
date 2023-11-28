b17fcca1cea22212acc097b443a1347986d13c6d
abk-gami <yahabubakar01@gmail.com>
2023-11-28 16:18:46 +0100

    l

---
+++ Jig.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Jig = ({name}) => {
+  return (
+    <View>
+      <Text>Hello, {name}</Text>
+    </View>
+  )
+}
+
+export default Jig
+
+const styles = StyleSheet.create({})
