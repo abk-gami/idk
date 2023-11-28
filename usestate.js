818f41683c3c9516206304aa178d5891a3149df3
abk-gami <yahabubakar01@gmail.com>
2023-11-28 14:41:36 +0100

    gh

---
+++ usestate.js
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View, ActivityIndicator} from 'react-native'
+import React, {useState} from 'react';
+import { StatusBar } from 'react-native';
+import { Button } from 'react-native';
+
+
+const usestate = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [showStatus, setShowStatus] = useState(false)
+  return (
+    <View style={{backgroundColor: '#ff0000', flex: 1,}}>
+      <StatusBar backgroundColor={'green'} barStyle={'default'}  hidden={showStatus}/>
+
+      <ActivityIndicator size={"large"} color={'black '} animating={isLoading}/>
+      
+      <Text style={{color: '#a9a9f3',}}>Hello World!</Text>
+  
+       <Button 
+       color={'#00ff73'}
+       title='Show'
+       onPress={()=>  setIsLoadingn(true)} 
+      />
+       <Button 
+       color={'#00ff73'}
+       title='Hide'
+       onPress={()=> setIsLoading(false)}
+      />
+ 
+
+
+
+    </View>
+  )
+}
+
+export default usestate
+
+const styles = StyleSheet.create({})
